0b064e1cf286227ce6273ae72ad0c7ff9a65b5b6
imran0dot <imran0dot@gmail.com>
2025-06-27 10:55:25 +0600

    download section and video section added

--- app/(pages)/(with-nav)/home/page.js
+++ app/(pages)/(with-nav)/home/page.js
@@ -7,10 +7,15 @@ import {
   WhatsNew,
 } from "../../../../sections";
 import ClientReview from "../../../../sections/ClientReview";
+import Video from "../../../../sections/Video";
 
 const Page = () => (
   <div className="mt-20">
     <Hero />
+    <div className="relative">
+      <Video />
+    </div>
+
     <div className="relative">
       <About />
       <div className="gradient-03 z-0" />
